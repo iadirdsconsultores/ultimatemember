f54d191cc7bad83cc14a3104a1d77e29748ed0a5
iadirdsconsultores <iadireccion@dsconsultores.es>
2025-06-04 08:45:24 -0500

    Update um-actions-login.php
    Hi Ultimate Member team 👋
    
    While reviewing the following authentication block
    
    is added in multiple places throughout the login flow — including in checks using authenticate and wp_authenticate_user.
    
    I believe this repetition is intentional for backward compatibility, but I’d like to suggest that including this message consistently is actually beneficial for user clarity. It reinforces that login failure may be due to an incorrect username/email, and not just an incorrect password.
    
    ✅ This improves user experience by reducing confusion, especially for users who might switch between usernames and email addresses when logging in.
    
    Thanks for your great work, and I appreciate your consideration of this detail!

--- includes/core/um-actions-login.php
+++ includes/core/um-actions-login.php
@@ -51,6 +51,7 @@ function um_submit_form_errors_hook_login( $submitted_data ) {
 	if ( $user && wp_check_password( $submitted_data['user_password'], $user->data->user_pass, $user->ID ) ) {
 		UM()->login()->auth_id = username_exists( $user_name );
 	} else {
+		UM()->form()->add_error( 'username', __( 'Please enter a correct username or email', 'ultimate-member' ) ); //added for backward compatibility
 		UM()->form()->add_error( 'user_password', __( 'Password is incorrect. Please try again.', 'ultimate-member' ) );
 	}
 
@@ -66,6 +67,7 @@ function um_submit_form_errors_hook_login( $submitted_data ) {
 		if ( ! empty( $third_party_codes ) && in_array( $user->get_error_code(), $third_party_codes ) ) {
 			UM()->form()->add_error( $user->get_error_code(), $user->get_error_message() );
 		} else {
+			UM()->form()->add_error( 'username', __( 'Please enter a correct username or email', 'ultimate-member' ) ); //added for backward compatibility
 			UM()->form()->add_error( 'user_password', __( 'Password is incorrect. Please try again.', 'ultimate-member' ) );
 		}
 	}
@@ -75,6 +77,7 @@ function um_submit_form_errors_hook_login( $submitted_data ) {
 		if ( ! empty( $third_party_codes ) && in_array( $user->get_error_code(), $third_party_codes ) ) {
 			UM()->form()->add_error( $user->get_error_code(), $user->get_error_message() );
 		} else {
+			UM()->form()->add_error( 'username', __( 'Please enter a correct username or email', 'ultimate-member' ) ); //added for backward compatibility
 			UM()->form()->add_error( 'user_password', __( 'Password is incorrect. Please try again.', 'ultimate-member' ) );
 		}
 	}
